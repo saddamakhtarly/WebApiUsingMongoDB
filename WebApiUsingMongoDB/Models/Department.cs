48e1aab583220a1c3e4c6830c1808be8d537a120
Saddam Akhtar <saddamakhtar.ly@outlook.com>
2021-07-02 19:20:32 +0500

    Add project files.

---
+++ WebApiUsingMongoDB/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiUsingMongoDB.Models
+{
+    public class Department
+    {
+        public ObjectId Id { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+
+
+    }
+}
